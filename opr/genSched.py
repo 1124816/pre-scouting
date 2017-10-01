485ddac9259d175bfdebfdde2a6bf21015cc1250
Elias Basler <e.e.basler@gmail.com>
2017-09-30 22:23:39 -0400

    Add scouting schedule

--- opr/genSched.py
+++ opr/genSched.py
@@ -1,12 +1,34 @@
 import tbapy
 
-tba = tbapy.TBA('w4sdTrGh4f4ueC0pfm5ZceCZp3yM3I0SRsJLrn1oblRowvr3Zx2UbcXQpaWnZSpK')
+tba = tbapy.TBA(input('key'))
 
 events = tba.event_matches('2017alhu')
+
+event = []
 
 for p in events:
     if p['comp_level'] == 'qm':
         event.append(p)
 
+final = []
+
 for i in event:
-    print(i)
+    #print(i.alliances['red']['team_keys'])
+    final.append(i.alliances['red']['team_keys'] + i.alliances['blue']['team_keys'])
+
+x = 0
+while x < len(final):
+    if 'frc4924' in final[x]:
+        y = x
+        team = {}
+        final[x].remove('frc4924')
+        for p in final[x]:
+            team[p] = 6
+        while y >= 0:
+            for i in team.keys():
+                if i in final[y] and team[i] > 0:
+                    print('scout team ' + i + ' in match ' + str(y+1))
+                    team[i]-=1
+            y-=1
+    x+=1
+
