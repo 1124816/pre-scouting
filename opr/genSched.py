2e6720c3088a5a1013c0742bea4d4e18ab625047
Elias Basler <e.e.basler@gmail.com>
2017-11-27 21:07:26 -0500

    Fix scouting schedule

--- opr/genSched.py
+++ opr/genSched.py
@@ -1,6 +1,7 @@
 import tbapy
+import os
 
-tba = tbapy.TBA(input('key'))
+tba = tbapy.TBA(os.environ['TBAKEY'])
 
 events = tba.event_matches('2017alhu')
 
@@ -10,25 +11,44 @@ for p in events:
     if p['comp_level'] == 'qm':
         event.append(p)
 
-final = []
+final = [0]*len(event)
 
 for i in event:
-    #print(i.alliances['red']['team_keys'])
-    final.append(i.alliances['red']['team_keys'] + i.alliances['blue']['team_keys'])
+    final[i['match_number']-1] = i.alliances['red']['team_keys'] + i.alliances['blue']['team_keys']
+#print(number)
+
+scoutlist = [0]*len(final)
 
 x = 0
 while x < len(final):
     if 'frc4924' in final[x]:
-        y = x
+        y = x-1
         team = {}
         final[x].remove('frc4924')
         for p in final[x]:
-            team[p] = 6
+            team[p] = 4
+        #print(team)
         while y >= 0:
-            for i in team.keys():
+            for i in final[x]:
                 if i in final[y] and team[i] > 0:
-                    print('scout team ' + i + ' in match ' + str(y+1))
+                    #print('scout team ' + i + ' in match ' + str(number[y]))
+                    #if y == 8:
+                    #    print(team)
+                    #    print(i)
+                    #    print(x)
+                    if scoutlist[y] == 0:
+                        scoutlist[y]=[i]
+                    else:
+                        scoutlist[y].append(i)
                     team[i]-=1
             y-=1
     x+=1
 
+maxscout = 0
+for i in scoutlist:
+    if i == 0:i=[]
+    if len(i) > maxscout:
+        maxscout = len(set(i))
+    print(len(set(i)))
+print(scoutlist)
+print("Max: " + str(maxscout))
